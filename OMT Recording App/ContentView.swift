0bf25371021017423a189850f1b01c7390bc5e71
harry <harryrmoore@gmail.com>
2024-09-12 22:07:44 +0545

    Initial Commit

---
+++ OMT Recording App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OMT Recording App
+//
+//  Created by Harry Moore on 12/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OMT Recording App/OMT_Recording_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OMT_Recording_AppApp.swift
+//  OMT Recording App
+//
+//  Created by Harry Moore on 12/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct OMT_Recording_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
